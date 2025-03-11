c47be3d0e022d385f0a3a806cc26cd60bf1c8a4e
Vikas Jaiswal <vjvikas66@gmail.com>
2025-03-11 19:59:16 +0530

    keyword updated

--- ReferenceOperator/KeyPropertiesOfReference.cpp
+++ ReferenceOperator/KeyPropertiesOfReference.cpp
@@ -1,19 +1,19 @@
 /*
 1. Must be initialized:-
- A reference can not reference without an initialize variable. it is just a alias
- // int& ref; 
- // Error: Reference must be iniialized
+A reference can not reference without an initialize variable. it is just a alias
+// int& ref; 
+// Error: Reference must be iniialized
 
- 2. No null Reference
-   Unlike pointer, reference can not be null reference
+2. No null Reference
+Unlike pointer, reference can not be null reference
 
- 3. Can not be reassigned
- int x = 10, y = 20;
- int& ref = x;
- ref = y; // Assign's Y value to x(x becomes 20)
- //ref still refer to x, not y.alignas
+3. Can not be reassigned
+int x = 10, y = 20;
+int& ref = x;
+ref = y; // Assign's Y value to x(x becomes 20)
+//ref still refer to x, not y.alias
 
- 4. No memory overhead
- // References don't occupy the seprate memory
+4. No memory overhead
+// References don't occupy the separate memory
  
 */
